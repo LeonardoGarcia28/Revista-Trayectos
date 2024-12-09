95b6eea19ee1922f92cafee920833d473dc59d9f
LeonardoGarcia28 <lgarciafuentes28@hotmail.com>
2024-12-09 05:42:48 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flyonui/dist/js/*.js",
+    "./node_modules/flyonui/dist/js/accordion.js"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flyonui"),
+    require("flyonui/plugin")
+  ],
+}
