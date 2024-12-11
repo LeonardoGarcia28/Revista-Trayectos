a58383ac546b66ec1b94bf40255ef03eb489c27c
LeonardoGarcia28 <lgarciafuentes28@hotmail.com>
2024-12-11 05:44:57 -0500

    Icons change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
+const { addDynamicIconSelectors } = require("@iconify/tailwind")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/*.html",
     "./src/**/*.{html,js}",
     "./node_modules/flyonui/dist/js/*.js",
-    "./node_modules/flyonui/dist/js/accordion.js"
+    "./node_modules/flyonui/dist/js/accordion.js",
+    "./src/**/*.{html,js,jsx,tsx}"
 ],
   theme: {
     extend: {},
   },
   plugins: [
     require("flyonui"),
-    require("flyonui/plugin")
+    require("flyonui/plugin"),
+    addIconSelectors(['mdi', 'mdi-light']),
+    addDynamicIconSelectors()
   ],
 }
